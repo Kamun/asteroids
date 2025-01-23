0476938b228cf23b2738ae482603d6bb0e3a63c7
Kamun <z30993@hotmail.com>
2025-01-23 00:00:08 -0500

    working rotation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 gameclock = pygame.time.Clock()
 dt = 0
@@ -13,14 +14,21 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
 	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: # if user clicks 'x' while game is running, game will close
+				pygame.quit()
 				return
 		screen.fill((0,0,0)) # Fill screen with black
-		pygame.display.flip() # Refresh screen
+		player.draw(screen)
 		dt = gameclock.tick(60) / 1000
+		player.update(dt)
+		pygame.display.flip() # Refresh screen
+		
 		
 
 if __name__ == "__main__":
